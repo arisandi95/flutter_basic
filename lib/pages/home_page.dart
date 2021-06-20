5b8764ef63567b4b8372be5c5a1f6e720d271943
sands <muhammadarisandi95@gmail.com>
2021-06-20 17:28:34 +0700

    modify home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,84 +10,169 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: backgroundColor,
       body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 50,
-              ),
-              Text(
-                'Profile Picture',
-                style: TextStyle(
-                  color: primaryColor,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600,
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 50,
                 ),
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              Image.asset(
-                'assets/images/primary.png',
-                width: 140,
-                // height: 140,
-                // fit: BoxFit.fill
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              Text(
-                'Muhammad Arisandi',
-                style: TextStyle(
-                  color: primaryColor,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500,
+                Text(
+                  'Profile Picture',
+                  style: TextStyle(
+                    color: primaryColor,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 4,
-              ),
-              Text(
-                'Web Developer',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: greyColor
-                )
-              ),
-              SizedBox(
-                height: 70,
-              ),
-              Wrap(
-                spacing: 38,
-                runSpacing: 40,
-                children: [
-                  Image.asset(
-                    'assets/images/item1.png',
-                    width: 80,
+                SizedBox(
+                  height: 50,
+                ),
+                Image.asset(
+                  'assets/images/primary.png',
+                  width: 140,
+                  // height: 140,
+                  // fit: BoxFit.fill
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  'Muhammad Arisandi',
+                  style: TextStyle(
+                    color: primaryColor,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
                   ),
-                  Image.asset(
-                    'assets/images/item2.png',
-                    width: 80,
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+                Text(
+                  'Web Developer',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: greyColor
+                  )
+                ),
+                SizedBox(
+                  height: 70,
+                ),
+                Wrap(
+                  spacing: 38,
+                  runSpacing: 40,
+                  children: [
+                    Image.asset(
+                      'assets/images/item1.png',
+                      width: 80,
+                    ),
+                    Image.asset(
+                      'assets/images/item2.png',
+                      width: 80,
+                    ),
+                    Image.asset(
+                      'assets/images/item3.png',
+                      width: 80,
+                    ),
+                     Image.asset(
+                      'assets/images/item4.png',
+                      width: 80,
+                    ),
+                    Image.asset(
+                      'assets/images/item5.png',
+                      width: 80,
+                    ),
+                    Image.asset(
+                      'assets/images/item6.png',
+                      width: 80,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 70,
+                ),
+                Container(
+                  color: Colors.white,
+                  width: 224,
+                  height: 50,
+                  child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16)
+                    ),
+                    onPressed: (){
+                      showModalBottomSheet(
+                        context: context,
+                        builder: (context) {
+                          return Container(
+                            height: 290,
+                            padding: EdgeInsets.symmetric(
+                              vertical: 50
+                            ),
+                            color: Colors.white,
+                            child: Column(
+                              children: [
+                                Text(
+                                  'Update Photo',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600,
+                                    color: primaryColor,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 12,
+                                ),
+                                Text(
+                                  'You are only able to change \n the picture profile once',
+                                  style: TextStyle(
+                                    color: greyColor,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w400
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                                SizedBox(
+                                  height: 30,
+                                ),
+                                Container(
+                                  width: 224,
+                                  height: 55,
+                                  child: RaisedButton(
+                                    color: orangeColor,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(16)
+                                    ),
+                                    onPressed: () {},
+                                    child: Text(
+                                      'Continue',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w500
+                                      ),
+                                    ),
+                                  )
+                                )
+                              ],
+                            ),
+                          );
+                        },
+                      );
+                    },
+                    child: Text(
+                      'Update Profile',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500
+                      )
+                    ),
                   ),
-                  Image.asset(
-                    'assets/images/item3.png',
-                    width: 80,
-                  ),
-                   Image.asset(
-                    'assets/images/item4.png',
-                    width: 80,
-                  ),
-                  Image.asset(
-                    'assets/images/item5.png',
-                    width: 80,
-                  ),
-                  Image.asset(
-                    'assets/images/item6.png',
-                    width: 80,
-                  ),
-                ],
-              ),
-            ],
+                ),
+                SizedBox(
+                  height: 70,
+                ),
+              ],
+            ),
           ),
         ),
       ),
